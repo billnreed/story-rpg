50702eaac6cf2f6a2049294e8738951284d689ef
Bill Reed <billnreed@gmail.com>
2017-02-02 16:53:49 -0500

    basic react + webpack2

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  niject: 'body'
+});
+
+module.exports = {
+  entry: './src/scripts/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    loaders: [
+      { test: /\.jsx?$/, loader:'babel-loader', exclude: /node_modules/ }
+    ]
+  },
+  plugins: [
+    HtmlWebpackPluginConfig
+  ]
+};
